5da3da350869c3de0df9914afd982a6dc3f441ae
spde3289 <spde3289@naver.com>
2025-02-15 14:09:46 +0900

    style: 글로벌 CSS 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,10 @@ import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
 const NotoSansKR = Noto_Sans_KR({
-  subsets: ["latin"], // 사용할 언어 서브셋
-  weight: ["300", "400", "500", "600", "700"], // 사용할 폰트 두께
-  style: ["normal"], // 사용할 스타일
-  display: "swap", // 폰트 로딩 전략
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  style: ["normal"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
